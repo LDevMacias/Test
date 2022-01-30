340c2dcb3e838f8c240783d7f31ebe1d264dba9b
LDevMacias <84604191+LDevMacias@users.noreply.github.com>
2022-01-30 15:04:38 -0500

    Update calculadora.py
    Añadida las operaciones de la calculadora

--- calculadora.py
+++ calculadora.py
@@ -1,1 +1,33 @@
-aaa
+class Operaciones:
+    def __init__(self):
+        self.resultado = 0
+
+    def suma(self, numero_1, numero_2):
+        self.resultado = numero_1 + numero_2
+        print(self.resultado)
+
+    def resta(self, numero_1, numero_2):
+        self.resultado = numero_1 - numero_2
+        print(self.resultado)
+
+    def multipica(self, numero_1, numero_2):
+        self.resultado = numero_1 * numero_2
+        print(self.resultado)
+
+    def divide(self, numero_1, numero_2):
+        self.resultado = numero_1 / numero_2
+        print(self.resultado)
+
+    def pontenciacion(self, numero, potencia):
+        self.resultado = numero ** potencia
+        print(self.resultado)
+
+    def radicacion(self, numero, raiz):
+        self.resultado = numero ** (1/raiz)
+        print(self.resultado)
+
+    def raiz_cuadrada(self, numero):
+        self.radicacion(numero, 2)
+
+    def raiz_cubica(self, numero):
+        self.radicacion(numero, 3)
